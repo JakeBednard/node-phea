b698a98dc28afed8635f6aa4b9e8dd608d5a1528
Jake Bednard <jacob.bednard@gmail.com>
2019-10-19 13:48:58 -0500

    FIxed name spacing issue with Phea

--- build/phea.js
+++ build/phea.js
@@ -14,73 +14,70 @@ const hue_bridge_1 = require("./hue-bridge");
 const hue_http_1 = require("./hue-http");
 const phea_config_1 = require("./phea-config");
 const logger = log4js_api_1.getLogger('PHEA');
-var Phea;
-(function (Phea) {
-    function discover() {
-        return __awaiter(this, void 0, void 0, function* () {
-            let bridges = yield hue_http_1.HueHttp.discoverBridge();
-            return bridges;
-        });
+function discover() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let bridges = yield hue_http_1.HueHttp.discoverBridge();
+        return bridges;
+    });
+}
+exports.discover = discover;
+function register(ipAddress) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let credentials = yield hue_http_1.HueHttp.registration(ipAddress);
+        return credentials;
+    });
+}
+exports.register = register;
+function bridge(options) {
+    return __awaiter(this, void 0, void 0, function* () {
+        configureBridgeOptions(options);
+        let bridge = new hue_bridge_1.HueBridge(options);
+        return bridge;
+    });
+}
+exports.bridge = bridge;
+function configureBridgeOptions(options) {
+    if (options == null || typeof (options) !== 'object')
+        throw new Error("PHEA [Configuration]: No (or incorrect) configuration dictionary object given. Please see documented examples.");
+    if (typeof (options.address) !== 'string')
+        throw new Error("PHEA [Configuration]: 'options.address' type invalid or unspecified. 'address' must be a string containing IP in IPv4 format or domain. Can contain port.");
+    if (typeof (options.username) !== 'string')
+        throw new Error("PHEA [Configuration]: 'options.username' type invalid or unspecified. Must be string.");
+    if (typeof (options.psk) !== 'string')
+        throw new Error("PHEA [Configuration]: 'options.psk' type invalid or unspecified. Must be string.");
+    if (options.dtlsUpdatesPerSecond == null) {
+        options.dtlsUpdatesPerSecond = phea_config_1.Config.DTLS_UPDATES_PER_SECOND;
     }
-    Phea.discover = discover;
-    function register(ipAddress) {
-        return __awaiter(this, void 0, void 0, function* () {
-            let credentials = yield hue_http_1.HueHttp.registration(ipAddress);
-            return credentials;
-        });
+    else if (typeof (options.dtlsUpdatesPerSecond) !== 'number' ||
+        options.dtlsUpdatesPerSecond < phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MIN ||
+        options.dtlsUpdatesPerSecond > phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MAX) {
+        throw new Error("PHEA [Configuration]: 'options.dtlsUpdatesPerSecond' must be of type int between " +
+            phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MIN + " and " + phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MAX + " inclusive.");
     }
-    Phea.register = register;
-    function bridge(options) {
-        return __awaiter(this, void 0, void 0, function* () {
-            configureBridgeOptions(options);
-            let bridge = new hue_bridge_1.HueBridge(options);
-            return bridge;
-        });
+    if (options.colorUpdatesPerSecond == null) {
+        options.colorUpdatesPerSecond = phea_config_1.Config.COLOR_UPDATES_PER_SECOND;
     }
-    Phea.bridge = bridge;
-    function configureBridgeOptions(options) {
-        if (options == null || typeof (options) !== 'object')
-            throw new Error("PHEA [Configuration]: No (or incorrect) configuration dictionary object given. Please see documented examples.");
-        if (typeof (options.address) !== 'string')
-            throw new Error("PHEA [Configuration]: 'options.address' type invalid or unspecified. 'address' must be a string containing IP in IPv4 format or domain. Can contain port.");
-        if (typeof (options.username) !== 'string')
-            throw new Error("PHEA [Configuration]: 'options.username' type invalid or unspecified. Must be string.");
-        if (typeof (options.psk) !== 'string')
-            throw new Error("PHEA [Configuration]: 'options.psk' type invalid or unspecified. Must be string.");
-        if (options.dtlsUpdatesPerSecond == null) {
-            options.dtlsUpdatesPerSecond = phea_config_1.Config.DTLS_UPDATES_PER_SECOND;
-        }
-        else if (typeof (options.dtlsUpdatesPerSecond) !== 'number' ||
-            options.dtlsUpdatesPerSecond < phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MIN ||
-            options.dtlsUpdatesPerSecond > phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MAX) {
-            throw new Error("PHEA [Configuration]: 'options.dtlsUpdatesPerSecond' must be of type int between " +
-                phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MIN + " and " + phea_config_1.Config.DTLS_UPDATES_PER_SECOND_MAX + " inclusive.");
-        }
-        if (options.colorUpdatesPerSecond == null) {
-            options.colorUpdatesPerSecond = phea_config_1.Config.COLOR_UPDATES_PER_SECOND;
-        }
-        else if (typeof (options.colorUpdatesPerSecond) !== 'number' || options.colorUpdatesPerSecond < phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MIN ||
-            options.colorUpdatesPerSecond > phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MAX) {
-            throw new Error("PHEA [Configuration]: 'colorUpdatesPerSecond' must be of type int between " +
-                phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MIN + " and " + phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MAX + " inclusive.");
-        }
-        if (options.dtlsPort == null) {
-            options.dtlsPort = phea_config_1.Config.DTLS_PORT;
-        }
-        else if (typeof (options.dtlsPort) !== 'number' || options.dtlsPort < phea_config_1.Config.DTLS_PORT_MIN ||
-            options.dtlsPort > phea_config_1.Config.DTLS_PORT_MAX) {
-            throw new Error("PHEA [Configuration]: 'dtlsPort' must be of type int between " +
-                phea_config_1.Config.DTLS_PORT_MIN + " and " + phea_config_1.Config.DTLS_PORT_MAX + " inclusive.");
-        }
-        if (options.dtlsTimeoutMs == null) {
-            options.dtlsTimeoutMs = phea_config_1.Config.DTLS_TIMEOUT;
-        }
-        else if (typeof (options.dtlsTimeoutMs) !== 'number' || options.dtlsTimeoutMs < phea_config_1.Config.DTLS_TIMEOUT_MIN ||
-            options.dtlsTimeoutMs > phea_config_1.Config.DTLS_TIMEOUT_MAX) {
-            throw new Error("PHEA [Configuration]: 'dtlsTimeout' must be of type int between " +
-                phea_config_1.Config.DTLS_TIMEOUT_MIN + " and " + phea_config_1.Config.DTLS_TIMEOUT_MAX + " inclusive.");
-        }
+    else if (typeof (options.colorUpdatesPerSecond) !== 'number' || options.colorUpdatesPerSecond < phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MIN ||
+        options.colorUpdatesPerSecond > phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MAX) {
+        throw new Error("PHEA [Configuration]: 'colorUpdatesPerSecond' must be of type int between " +
+            phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MIN + " and " + phea_config_1.Config.COLOR_UPDATES_PER_SECOND_MAX + " inclusive.");
     }
-    Phea.configureBridgeOptions = configureBridgeOptions;
-})(Phea = exports.Phea || (exports.Phea = {}));
+    if (options.dtlsPort == null) {
+        options.dtlsPort = phea_config_1.Config.DTLS_PORT;
+    }
+    else if (typeof (options.dtlsPort) !== 'number' || options.dtlsPort < phea_config_1.Config.DTLS_PORT_MIN ||
+        options.dtlsPort > phea_config_1.Config.DTLS_PORT_MAX) {
+        throw new Error("PHEA [Configuration]: 'dtlsPort' must be of type int between " +
+            phea_config_1.Config.DTLS_PORT_MIN + " and " + phea_config_1.Config.DTLS_PORT_MAX + " inclusive.");
+    }
+    if (options.dtlsTimeoutMs == null) {
+        options.dtlsTimeoutMs = phea_config_1.Config.DTLS_TIMEOUT;
+    }
+    else if (typeof (options.dtlsTimeoutMs) !== 'number' || options.dtlsTimeoutMs < phea_config_1.Config.DTLS_TIMEOUT_MIN ||
+        options.dtlsTimeoutMs > phea_config_1.Config.DTLS_TIMEOUT_MAX) {
+        throw new Error("PHEA [Configuration]: 'dtlsTimeout' must be of type int between " +
+            phea_config_1.Config.DTLS_TIMEOUT_MIN + " and " + phea_config_1.Config.DTLS_TIMEOUT_MAX + " inclusive.");
+    }
+}
+exports.configureBridgeOptions = configureBridgeOptions;
 //# sourceMappingURL=../src/build/phea.js.map
